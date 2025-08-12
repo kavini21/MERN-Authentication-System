5db56158574e1a1646bb681a4677258f710f792e
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-08-12 07:47:38 +0530

    start backend

---
+++ server/controllers/authController.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.json({ success: false, message: "Missing Details" })
+    }
+
+    try {
+        const existingUser = await userModel.findOne({ email })
+        if (existingUser) {
+            return res.json({ success: false, message: "User Already Exists" })
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({
+            name,
+            email,
+            password: hashPassword
+        });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    }catch(error){
+        res.json({ success: false, message: error.message })
+    }
+
+    }
+
