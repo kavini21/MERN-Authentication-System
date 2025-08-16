e9db1ad75b3217938682a89247ba0bcdf542f96c
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-08-16 06:12:37 +0530

    add reutes to server file

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,15 +24,77 @@ export const register = async (req, res) => {
         await user.save();
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-        res.cookie('token' , token)
+        res.cookie('token' , token,
         {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production'
+            secure: process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge : 7 * 24 * 60 * 1000
+
         });
+
+        return res.jdon ({success: true});
+
 
     }catch(error){
         res.json({ success: false, message: error.message })
     }
 
+    }
+    export const login = async (ewq, res) =>{
+        const{email, password} = req.body;
+
+        if(!email || !password){
+            return res.json ({success: false, message:'Email and password are required'})
+        }
+
+        try {
+            const user = awit userModel.findOne({email});
+
+            if(!user){
+                return res.json({success: false, message: 'Invalid email'})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(isMatch){
+                return res.json({success: false, message: 'Invalid password'})
+            }
+
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token' , token,
+        {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge : 7 * 24 * 60 * 1000
+
+        });
+
+        return res.jdon ({success: true});
+
+
+
+        } catch (error) {
+            return res.json ({ success: false, message: error.message});
+        }
+
+    }
+
+    export const logout = async (req, res)=>{
+        try {
+            res.clearcookie('token', {
+                httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+
+            })
+            
+            return res.json({success: true, message: "Logged Out"})
+
+
+        } catch (error) {
+            return res.json({success: false, message: error.message});
+        }
     }
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
 import connectDB from './config/mongodb.js';
+import authRouter from '/routes/authRouter.js'
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
+//API Endpoints
 app.get('/', (req, res) => res.send("API Working "));
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => console.log(`server started on PORT: ${port}`));
